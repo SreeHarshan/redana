30bab889931dbf989af9069c271a8a2046a17b32
SreeHarshan <sreeerode12@gmail.com>
2023-08-21 22:50:12 +0530

    removed key

---
+++ app/lib/Cart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatefulWidget {
+  const Cart({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _cart createState() => _cart();
+}
+
+// ignore: camel_case_types
+class _cart extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
